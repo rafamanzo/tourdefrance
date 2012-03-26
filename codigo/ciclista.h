289688f8f1f02106199841afaf76e382246e6885
Giancarlo Rigo <gian@gian-laptop.(none)>
2012-03-26 09:13:08 -0300

    header do ciclista

---
+++ codigo/ciclista.h
@@ -0,0 +1,10 @@
+typedef struct cic{
+	unsigned int id;
+	double tmp;
+	double pos;
+	double vp; /* Velocidade no plano */
+	double vs; /* Velocidade na subida */
+	double vd; /* Velocidade na descida */
+} ciclista;
+
+
