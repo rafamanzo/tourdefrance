97f167f7f9505740b4ce46be92dcbd8572b390e5
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2012-03-29 13:02:25 -0300

    Varivel para largura da pista

--- codigo/estrada.h
+++ codigo/estrada.h
@@ -6,8 +6,9 @@ unsigned int prox_sinc_temp;
 unsigned short **estrada;
 char *tipo_trecho;
 unsigned int *tempo;
+unsigned short largura;
 
 pthread_mutex_t temp_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void inic_estrada(int num_cic, int dist);
-void set_prox_sinc_temp(unsigned int temp)
+void set_prox_sinc_temp(unsigned int temp);
