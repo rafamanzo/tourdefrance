27ce0a285f1b8173ab61a03241e4a6a6db483dca
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2012-03-29 09:38:14 -0300

    Ciclista se movendo na estrada sem checar condicoes

--- codigo/ciclista.c
+++ codigo/ciclista.c
@@ -5,8 +5,29 @@
 #include "ciclista.h"
 
 void loop(ciclista c){
-  while(floor(c.dist) < max_dist){
+  int pos_atual, prox_pos;
+
+  pos_atual = floor(c.dist);
+  
+  while(pos_atual < max_dist){
+    switch(tipo_trecho[pos_atual] == PLANO){
+      case SUBIDA:
+        c.dist += c.vp;
+        break;
+      case DESCIDA:
+        c.dist += c.vd;
+        break;
+      case PLANO:
+        c.dist += c.vp;
+        break;
+    }
     
+    prox_pos = floor(c.dist);
+    
+    if(prox_pos > pos_atual){
+      estrada[pos_atual][c.id] = 0;
+      estrada[prox_pos][c.id] = 1;
+    }
   }
   
   pthread_exit();
