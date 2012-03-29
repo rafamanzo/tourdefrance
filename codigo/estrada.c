9aa4b77b5fc1db6b9549cf4b34721f8314c7a2b7
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2012-03-29 08:11:07 -0300

    Estrutura de estrada criada junto com inicializacao

--- codigo/estrada.c
+++ codigo/estrada.c
@@ -1,8 +1,27 @@
+#include<stdlib.h>
+#include "estrada.h"
+
+void inic_estrada(int num_cic, int dist){
+  int i, j;
+
+  prox_sinc_temp = prox_sinc_dist = 0;
+
+  estrada = malloc(dist*sizeof(short *));
+  for(i = 0; i < dist; i++)
+    estrada[i] = malloc(num_cic*sizeof(short));
+    
+  for(i = 0; i < dist; i++)
+    for(j = 0; j < num_cic; j++)
+      if( i == 0 )
+        estrada[i][j] = 1;
+      else
+        estrada[i][j] = 0;
+}
 
 void insere(etapa* lst, char c, double k){
 	etapa* new;
 	new = malloc(sizeof(etapa));	
-	new.tipo = c;
+	new.type = c;
 	new.dist = k;
 	new.prox = NULL;
 	new.ult = NULL;	
--- codigo/estrada.h
+++ codigo/estrada.h
@@ -2,5 +2,12 @@ typedef struct et{
 	char tipo;
 	double dist;
 	et* prox;
-	et* last;
+	et* ult;
 } etapa;
+
+unsigned int prox_sinc_temp;
+unsigned int prox_sinc_dist;
+unsigned short **estrada;
+
+void inic_estrada(int num_cic, int dist);
+void insere(etapa* lst, char c, double k);
